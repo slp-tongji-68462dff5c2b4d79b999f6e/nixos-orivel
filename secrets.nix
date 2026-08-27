b2430caa656c103222fa779619c5c9fbe67fff57
yueyinqiu <yueyinqiu@outlook.com>
2026-08-27 17:50:10 +0800

    f

--- secrets.nix
+++ secrets.nix
@@ -1,7 +1,12 @@
 let
-  keys = [
-    # yueyinqiu@earth-latitude-7490
+  authorizedKeys = [
+    # yueyinqiu
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc1wIB537oVrGzzolKRX1Yfp0fKoUSg4pQRFxiUZyDF"
+
+    # 以下密钥是 glass-break 密钥，用以避免所有私钥均丢失导致 age 文件无法访问。
+    # 私钥提供在 GitHub 仓库的 agenix-glass-break 环境中。
+    # 必要时，可以在 GitHub Action 中使用它，调用 agenix --rekey 来添加新的可信密钥。
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIMMiZs+QRQsjtGtvBqOH5A8HDKzjbXgkodjfGiRhVy0"
   ];
 
   root = builtins.path {
@@ -35,10 +40,10 @@ builtins.listToAttrs (
   map (p: {
     name = p;
     value = {
-      publicKeys = keys;
+      publicKeys = authorizedKeys;
     };
   }) ageFiles
 )
 // {
-  "sample-secret.age".publicKeys = keys;
+  "sample-secret.age".publicKeys = authorizedKeys;
 }
