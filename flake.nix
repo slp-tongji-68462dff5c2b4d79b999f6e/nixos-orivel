f17db33a56332749ca91fdf706209d401aef3ea9
yueyinqiu <yueyinqiu@outlook.com>
2026-08-27 11:08:22 +0800

    2604

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-unstable";
+      url = "github:NixOS/nixpkgs/nixos-26.04";
     };
     nur = {
       url = "github:nix-community/NUR";
