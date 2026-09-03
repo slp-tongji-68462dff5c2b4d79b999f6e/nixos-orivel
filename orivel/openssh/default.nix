e7da3d8e24506e43342fa36e39058e466648e9c1
yueyinqiu <yueyinqiu@outlook.com>
2026-09-03 16:40:32 +0800

    f

--- orivel/openssh/default.nix
+++ orivel/openssh/default.nix
@@ -3,4 +3,8 @@
 }:
 {
   services.openssh.enable = true;
+  users.users.root.openssh.authorizedKeys.keys = [
+    # yueyinqiu@earth-latitude-7490
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc1wIB537oVrGzzolKRX1Yfp0fKoUSg4pQRFxiUZyDF"
+  ];
 }
