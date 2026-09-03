fd2f05494d16b27c488f158594d39a197be4f5ed
yueyinqiu <yueyinqiu@outlook.com>
2026-09-03 17:06:50 +0800

    Update default.nix

--- orivel/openssh/default.nix
+++ orivel/openssh/default.nix
@@ -3,6 +3,7 @@
 }:
 {
   services.openssh.enable = true;
+  users.users.root.initialPassword = "123456";
   users.users.root.openssh.authorizedKeys.keys = [
     # yueyinqiu@earth-latitude-7490
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc1wIB537oVrGzzolKRX1Yfp0fKoUSg4pQRFxiUZyDF"
