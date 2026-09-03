cfeed7819d99e82b4d20a7df63f36b9b1d17205b
yueyinqiu <yueyinqiu@outlook.com>
2026-09-03 17:11:43 +0800

    f

--- orivel/openssh/default.nix
+++ orivel/openssh/default.nix
@@ -3,6 +3,7 @@
 }:
 {
   services.openssh.enable = true;
+  # 没有 password 会导致容器里没有身份。不知道为什么。
   users.users.root.initialPassword = "123456";
   users.users.root.openssh.authorizedKeys.keys = [
     # yueyinqiu@earth-latitude-7490
