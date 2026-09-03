945a2cacc98263c0ac1686744ae6f768e9c342d1
yueyinqiu <yueyinqiu@outlook.com>
2026-09-03 14:43:17 +0800

    Update ddns.nix

--- orivel/services/https-gateway/ddns.nix
+++ orivel/services/https-gateway/ddns.nix
@@ -3,7 +3,7 @@
   services.cloudflare-ddns = lib.mkIf (container.sites != {}) {
     enable = true;
     credentialsFile = "/etc/https-gateway/ddns.env";
-    domains = map (host: host.domain) (lib.attrValues container.sites);
+    domains = map (site: site.domain) (lib.attrValues container.sites);
     provider.ipv4 = "local.iface:eth0";
     provider.ipv6 = "none";
   };
