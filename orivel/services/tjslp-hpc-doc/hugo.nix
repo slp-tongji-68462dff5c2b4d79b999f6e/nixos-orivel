bac9fa0785a46d84c7737d40be3f13f0e58b6f14
yueyinqiu <yueyinqiu@outlook.com>
2026-09-04 23:26:12 +0800

    Update hugo.nix

--- orivel/services/tjslp-hpc-doc/hugo.nix
+++ orivel/services/tjslp-hpc-doc/hugo.nix
@@ -4,29 +4,28 @@
   ...
 }:
 let
-  sourceDirectory = "/var/lib/${serviceConfigurations.stateDirectoryName}/hugo/source";
-  knownHosts = "/var/lib/${serviceConfigurations.stateDirectoryName}/hugo/known_hosts";
+  hugoDirectory = "/var/lib/${serviceConfigurations.stateDirectoryName}/hugo";
+  sourceDirectory = "${hugoDirectory}/source";
 
   script = pkgs.writeShellScript "tjslp-hpc-doc-pull-and-build" ''
     set -euo pipefail
 
-    export GIT_SSH_COMMAND="${pkgs.openssh}/bin/ssh -o StrictHostKeyChecking=accept-new -o UserKnownHostsFile=${knownHosts} -i ${serviceConfigurations.deployKeyFile}"
-
-    mkdir -p "$(dirname "${knownHosts}")"
-
+    mkdir -p "${hugoDirectory}"
+    
+    export GIT_SSH_COMMAND=\
+      '"${pkgs.openssh}/bin/ssh" -o StrictHostKeyChecking=accept-new -o UserKnownHostsFile="${hugoDirectory}/known_hosts" -i "${serviceConfigurations.deployKeyFile}"'
     if [ ! -d "${sourceDirectory}/.git" ]; then
-      ${pkgs.git}/bin/git clone \
+      "${pkgs.git}/bin/git" clone \
         --depth 1 \
         --branch main \
         git@github.com:yueyinqiu/TjslpHpcHandbook.git \
         "${sourceDirectory}"
     else
-      ${pkgs.git}/bin/git -C "${sourceDirectory}" fetch --depth 1 origin main
-      ${pkgs.git}/bin/git -C "${sourceDirectory}" reset --hard FETCH_HEAD
+      "${pkgs.git}/bin/git" -C "${sourceDirectory}" fetch --depth 1 origin main
+      "${pkgs.git}/bin/git" -C "${sourceDirectory}" reset --hard FETCH_HEAD
     fi
 
-    ${pkgs.git}/bin/git -C "${sourceDirectory}" submodule update --init --recursive
-
+    mkdir -p "${serviceConfigurations.outputDirectory}"
     ${pkgs.hugo}/bin/hugo \
       --source "${sourceDirectory}" \
       --destination "${serviceConfigurations.outputDirectory}"
